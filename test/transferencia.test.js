d09c099fa11eb14802f4bba96883394c8d144cdd
Erika-Bernardo <erika_bj_kk@hotmail.com>
2026-04-09 13:25:17 -0300

    Adicionando testes para get transferencias total e individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,13 +5,13 @@ const { obterToken } = require('../helpers/autenticacao');
 const postTransferencias = require('../fixtures/postTransferencias.json');
 
 describe('Transferências', () => {
+    let token;
+
+    beforeEach(async () => {
+        token = await obterToken('julio.lima', '123456');
+    });
+
     describe('POST/transferencias', () => {
-        let token;
-
-        beforeEach(async () => {
-            token = await obterToken('julio.lima', '123456');
-        });
-
 
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de 10,00', async () => {
             const bodyTransferencia = { ...postTransferencias };
@@ -36,5 +36,37 @@ describe('Transferências', () => {
             expect(resposta.status).to.equal(422);
         });
 
+    });
+
+    describe('GET/transferencias/{id}', () => {
+
+        it('Deve retornar sucesso com 200 e dados iguais ao registro de tranferência contido no banco de dados quando o id for válido', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias/13')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body.id).to.equal(13);
+            expect(resposta.body.id).to.be.a('number');
+            expect(resposta.body.valor).to.equal(11.00); // no swagger descrimina que é umnumer float, mas no banco de dados é string, necessario corrigir esse bug.
+            expect(resposta.body.conta_oriegm_id).to.equal(1)
+            expect(resposta.body.conta_destino_id).to.equal(2);
+        });
+    });
+
+    describe('GET/transferencias', () => {
+
+        it('Deve retornar 10 elementos na paginação quando informar limite de 10 registros', async () => {
+            
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias?page=1&limite=10')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body.limit).to.equal(10);
+            expect(resposta.body.transferencias).to.have.lengthOf(10);
+        });
     });
 });
