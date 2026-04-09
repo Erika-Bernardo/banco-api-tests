37f7e2ba0d473b88a2015913b8622807a0ecee4c
Erika-Bernardo <erika_bj_kk@hotmail.com>
2026-04-09 12:32:58 -0300

    Adicionando fixtures para gerir os corpos das requisicoes

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,43 +2,37 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const { obterToken } = require('../helpers/autenticacao');
+const postTransferencias = require('../fixtures/postTransferencias.json');
 
 describe('Transferências', () => {
     describe('POST/transferencias', () => {
         let token;
 
         beforeEach(async () => {
-            token = await obterToken('julio.lima', '123456'); 
+            token = await obterToken('julio.lima', '123456');
         });
 
 
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de 10,00', async () => {
+            const bodyTransferencia = { ...postTransferencias };
 
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 11.00,
-                    token: ""
-                });
+                .send(bodyTransferencia);
             expect(resposta.status).to.equal(201);
         });
 
         it('Deve retornar erro com 422 quando o valor da transferência for abaixo ou igual a 10,00', async () => {
+            const bodyTransferencia = { ...postTransferencias };
+            bodyTransferencia.valor = 9.99;
 
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 9.99,
-                    token: ""
-                });
+                .send(bodyTransferencia);
             expect(resposta.status).to.equal(422);
         });
 
